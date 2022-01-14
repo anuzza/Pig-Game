185056c336f7c41e6d30a402d936765ba8f7a0bf
anuzza <anuja3580@gmail.com>
2022-01-13 18:23:49 -0600

    starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+//selecting elements
+const score0El = document.querySelector('#score--0');
+// # is for id and . is for classes
+
+const score1El = document.getElementById('score--1');
+// more faster than query selector
+
+const diceEl = document.querySelector('.dice');
+
+// starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
